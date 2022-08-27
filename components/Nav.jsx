a9b2811ebde8ac85098b2b776db7ce61bf3a165c
amittras-pal <pal.amittras@gmail.com>
2022-08-27 21:08:05 +0530

    added a scroll interactive navbar.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,32 @@ import {
   Group,
   Header,
   Text,
+  useMantineTheme,
 } from "@mantine/core";
 import { NextLink } from "@mantine/next";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { APP_TITLE } from "../constants/app.constants";
 import { useMediaMatch } from "../hooks/isMobile";
 
-function Nav() {
+export default function Nav() {
   const { pathname } = useRouter();
   const isMobile = useMediaMatch();
-  const { classes } = useStyles();
+  const { classes, cx } = useStyles();
+  const { colors } = useMantineTheme();
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const scrollHandler = (e) => {
+      const containerPosition = e.target.scrollingElement.scrollTop;
+      if (containerPosition >= 250) setScrolled(true);
+      else setScrolled(false);
+    };
+    document.addEventListener("scroll", scrollHandler);
+    return () => {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
 
   const [open, setOpen] = useState(false);
   const toggleDrawer = () => {
@@ -25,41 +40,26 @@ function Nav() {
 
   return (
     <>
-      <Header height={60} className={classes.header}>
+      <Header
+        className={cx(classes.header, {
+          [classes.scrolled]: scrolled,
+          [classes.unscrolled]: !scrolled,
+        })}>
         <Text
           component={NextLink}
           href="/"
-          weight="bold"
-          size="xl"
-          color="gray">
+          size="lg"
+          weight={500}
+          color={scrolled ? colors.gray[8] : colors.gray[6]}>
           {APP_TITLE}
         </Text>
-        {isMobile ? (
-          <Burger
-            opened={open}
-            onClick={toggleDrawer}
-            ml="auto"
-            size="sm"
-            color="gray"
-          />
-        ) : (
-          <Group ml="auto">
-            <Button
-              component={NextLink}
-              href="/stories"
-              color="indigo"
-              variant={pathname === "/stories" ? "filled" : "subtle"}>
-              Stories & Narratives
-            </Button>
-            <Button
-              component={NextLink}
-              href="/submissions"
-              color="indigo"
-              variant={pathname === "/submissions" ? "filled" : "subtle"}>
-              Submit your post
-            </Button>
-          </Group>
-        )}
+        <Burger
+          opened={open}
+          onClick={toggleDrawer}
+          ml="auto"
+          size="sm"
+          color="gray"
+        />
       </Header>
       <Drawer
         position="right"
@@ -67,26 +67,32 @@ function Nav() {
         onClose={toggleDrawer}
         withCloseButton
         title={APP_TITLE}
-        size="md"
+        size={isMobile ? "md" : "xl"}
         padding="md">
-        <Group mt={"2rem"}>
+        <Group mt={"2rem"} spacing="sm" sx={{ flexDirection: "column" }}>
           <Button
             component={NextLink}
             href="/stories"
             fullWidth
-            variant={pathname === "/stories" ? "gradient" : "subtle"}
-            onClick={toggleDrawer}
-            gradient={{ from: "orange", to: "red" }}>
+            variant={pathname === "/stories" ? "outline" : "subtle"}
+            onClick={toggleDrawer}>
             Stories & Narratives
+          </Button>
+          <Button
+            component={NextLink}
+            href="/stories"
+            fullWidth
+            variant={pathname === "/posts" ? "outline" : "subtle"}
+            onClick={toggleDrawer}>
+            Posts
           </Button>
           <Button
             component={NextLink}
             href="/submissions"
             fullWidth
-            variant={pathname === "/submissions" ? "gradient" : "subtle"}
-            onClick={toggleDrawer}
-            gradient={{ from: "orange", to: "red" }}>
-            Submit your post
+            variant={pathname === "/submissions" ? "outline" : "subtle"}
+            onClick={toggleDrawer}>
+            Submit your Work
           </Button>
         </Group>
       </Drawer>
@@ -94,20 +100,25 @@ function Nav() {
   );
 }
 
-export default Nav;
-
 const useStyles = createStyles((theme) => ({
   header: {
     display: "flex",
     position: "fixed",
-    top: "0",
+    top: 0,
     alignItems: "center",
     padding: theme.spacing.sm,
     flexGrow: 0,
     boxShadow: theme.shadows.sm,
-    // backgroundColor: "transparent",
-    backgroundColor: `${theme.white}0F`,
     borderColor: "transparent",
     backdropFilter: "blur(10px)",
+    transition: "all 0.3s ease-in-out",
+  },
+  scrolled: {
+    backgroundColor: `${theme.colors.gray[2]}CC`,
+    height: 50,
+  },
+  unscrolled: {
+    backgroundColor: `${theme.white}0F !important`,
+    height: 70,
   },
 }));
